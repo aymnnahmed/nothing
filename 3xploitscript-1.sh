4b125839536e96744d8d3fb89aced126bceb22f0
3xploit666 <30919412+3xploit666@users.noreply.github.com>
2017-08-12 06:37:23 +0200

    Add files via upload

---
+++ 3xploitscript-1.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#############################################
+echo sigueme en mi canal de youtobe 3xploit 
+##############################################
+apt install figlet 
+figlet 3xploit
+apt install sl 
+sl
+echo 3xploit 
+####################
+figlet nmap
+##################
+echo instalando nmap
+apt install nmap
+apt install python
+figlet python
+apt install php
+figlet php
+apt install python2
+#################
+echo instalando python2
+###################
+echo 3xploit termux suscribete a mi canal 
+figlet red_hawk
+git clone https://git@github.com:Tuhinshubhra/RED_HAWK.git
+git clone https://github.com/lightos/credmap.git
+figlet credmap
+git clone https://github.com/sqlmapproject/sqlmap.git
+figlet sqlmap
+apt install tor
+echo instalando tor
+apt install perl 
+apt install w3m 
+echo instalando w3m
+apt install apache2
+apt install cowsay
+cowsay 3xploit 
+figlet 3xploit
+###########################
+echo suscribete a mi canal
+###########################
