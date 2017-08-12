618078b573e088dcb9a0ad8710d6f215970c219f
3xploit666 <30919412+3xploit666@users.noreply.github.com>
2017-08-12 07:32:16 +0200

    Add files via upload

---
+++ scriptpack.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+"#############################################"
+echo sigueme en mi canal de youtobe 3xploit 
+"##############################################"
+apt install figlet 
+figlet 3xploit
+apt install sl 
+sl
+echo 3xploit 
+"####################"
+figlet nmap
+"#################"
+echo instalando nmap
+apt install nmap
+apt install python
+figlet python
+apt install php
+figlet php
+apt install python2
+"#################"
+echo instalando python2
+"###################"
+echo 3xploit termux suscribete a mi canal 
+figlet red_hawk
+git clone https://github.com/Tuhinshubhra/RED_HAWK.git
+figlet credmap
+git clone https://github.com/lightos/credmap.git
+figlet sqlmap
+git clone https://github.com/sqlmapproject/sqlmap.git
+figlet recondog 
+git clone https://github.com/UltimateHackers/ReconDog.git
+figlet ngrok 
+git clone https://github.com/inconshreveable/ngrok.git
+apt install tor
+echo instalando tor
+apt install perl 
+apt install w3m 
+echo instalando w3m
+apt install apache2
+apt install cowsay
+cowsay 3xploit 
+figlet 3xploit
+"###########################"
+echo suscribete a mi canal
+"###########################"
